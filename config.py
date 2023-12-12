0448eeb9146a30691b7c3c9f72ddcf7dfb037dff
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2023-12-12 21:15:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,16 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-h
+API_ID = int(os.getenv("API_ID", "27794931"))
+API_HASH = os.environ.get("API_HASH", "64498a08cd474a139a8e5a1c5a5aa776")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
+ADMIN = ADMINS
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://kailash:pass@cluster0.sqtztxm.mongodb.net/?retryWrites=true&w=majority") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "5851749250")) 
+ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
+ADMINS.append(5851749250)
 
 #  Optionnal variables
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001720210775")) 
